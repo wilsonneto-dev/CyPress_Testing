e617c867c78dd9e400e444ccabc4d4316ea3bfcf
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-01-29 13:06:34 -0300

    setup

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,8 @@
+describe('My first test', () => {
+  it('Does not do much', () => {
+    // Arrange - setup initial app state
+    // Act - take an action
+    // Assert - make an assertion
+    // expect(true).to.equal(false);
+  });
+});
