1f8b244d2a8ab350c02ad97fe183c4fcf5aab6c9
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-01-29 13:47:45 -0300

    new tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,18 @@
 describe('My first test', () => {
-  it('Does not do much', () => {
+  /* it('Does not do much', () => {
     // Arrange - setup initial app state
+    // - visit a web page
+    // - query a element
     // Act - take an action
+    // - interact with that element
     // Assert - make an assertion
-    // expect(true).to.equal(false);
+    // - make an assertion
+    // expect(true).to.equal(false); // simple example
+  }); */
+
+  it('Visits the Looke site, finds a element', () => {
+    cy.visit('https://www.looke.com.br');
+    cy.get('.gdl').click({ force: true });
+    cy.contains('Atire Duas').click({ force: true });
   });
 });
